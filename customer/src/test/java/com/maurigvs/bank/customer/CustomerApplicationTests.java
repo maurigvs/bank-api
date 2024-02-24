50772bf806d7891e8a1b39f9ee1c3c07c4c2160d
maurigvs <maurigvs@icloud.com>
2024-02-24 21:32:07 +0000

    initial commit

---
+++ customer/src/test/java/com/maurigvs/bank/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurigvs.bank.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
