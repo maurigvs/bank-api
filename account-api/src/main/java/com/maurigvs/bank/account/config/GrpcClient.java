e64a0ce6720ce1f8ce140bc3acd2b1462fd9f5af
maurigvs <maurigvs@icloud.com>
2024-03-08 10:59:19 +0000

    account-api grpc properties

--- account-api/src/main/java/com/maurigvs/bank/account/config/GrpcClient.java
+++ account-api/src/main/java/com/maurigvs/bank/account/config/GrpcClient.java
@@ -3,16 +3,23 @@ package com.maurigvs.bank.account.config;
 import com.maurigvs.bank.grpc.CustomerServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GrpcClient {
 
+    @Value("${bank.grpc-client.customer.host}")
+    private String host;
+
+    @Value("${bank.grpc-client.customer.port}")
+    private Integer port;
+
     @Bean
     public ManagedChannel managedChannel(){
         return ManagedChannelBuilder
-                .forAddress("localhost", 8182)
+                .forAddress(host, port)
                 .usePlaintext()
                 .build();
     }
