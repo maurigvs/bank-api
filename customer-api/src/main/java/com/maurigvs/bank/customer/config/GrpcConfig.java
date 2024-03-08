59e27310c56914de77d9c13ce2eddd5445666e0e
maurigvs <maurigvs@icloud.com>
2024-03-08 11:05:00 +0000

    customer-api grpc properties

--- customer-api/src/main/java/com/maurigvs/bank/customer/config/GrpcConfig.java
+++ customer-api/src/main/java/com/maurigvs/bank/customer/config/GrpcConfig.java
@@ -4,15 +4,19 @@ import com.maurigvs.bank.customer.grpc.CustomerGrpcService;
 import com.maurigvs.bank.customer.service.PersonService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GrpcConfig {
 
+    @Value("${bank.grpc-server.port}")
+    private Integer port;
+
     @Bean
     public Server serverBuilder(PersonService personService){
-        return ServerBuilder.forPort(8182)
+        return ServerBuilder.forPort(port)
                 .directExecutor()
                 .addService(new CustomerGrpcService(personService))
                 .build();
